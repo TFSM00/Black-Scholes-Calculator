817d7fe26cc6e6c829238b8f3e0ca616e6957545
TFSM00 <tiagomoreira317@gmail.com>
2022-02-17 13:42:40 +0000

    Started project and added its baseline code

---
+++ ASX_BSM.py
@@ -0,0 +1,182 @@
+import numpy as np
+from scipy.stats import norm
+from py_vollib.black_scholes import black_scholes as bs
+import py_vollib.black_scholes.greeks.analytical as bsg
+import matplotlib.pyplot as plt
+
+r = 0.01      # risk free
+S = 30        # underlying asset value
+K = 50        # strike price
+days_to_expiry = 10
+T = days_to_expiry/365   # time to expiration in year portions
+sigma = 0.3   # volatility
+type = "c"
+
+def blackScholes(S, K, r, T, sigma, type="c"):
+    "Calculate Black Scholes option price for a call/put"
+
+    d1 = (np.log(S/K) + (r + sigma**2/2)* T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    try:
+        if type == "c":
+            price = S * norm.cdf(d1, 0, 1) - K * np.exp(-r * T) * norm.cdf(d2, 0, 1)
+        elif type == "p":
+            price = K * np.exp(-r * T) * norm.cdf(-d2, 0, 1) - S * norm.cdf(-d1, 0, 1)
+
+        return price
+    except:  
+        print("Please confirm all option parameters!")
+
+
+def optionDelta (S, K, r, T, sigma, type="c"):
+    "Calculates option delta"
+    d1 = (np.log(S/K) + (r + sigma**2/2)* T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    try:
+        if type == "c":
+            delta = norm.cdf(d1, 0, 1)
+        elif type == "p":
+            delta = -norm.cdf(-d1, 0, 1)
+
+        return delta
+    except:
+        print("Please confirm all option parameters!")
+
+def optionGamma (S, K, r, T, sigma):
+    "Calculates option gamma"
+    d1 = (np.log(S/K) + (r + sigma**2/2)* T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    
+    try:
+        gamma = norm.pdf(d1, 0, 1)/ (S * sigma * np.sqrt(T))
+        return gamma
+    except:
+        print("Please confirm all option parameters!")
+
+def optionTheta(S, K, r, T, sigma, type="c"):
+    "Calculates option theta"
+    d1 = (np.log(S/K) + (r + sigma**2/2)* T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    try:
+        if type == "c":
+            theta = -S * (norm.pdf(d1, 0, 1) * sigma / (2 * np.sqrt(T))) - r * K * np.exp(-r*T) * norm.cdf(d2, 0, 1)
+
+        elif type == "p":
+            theta = -S * (norm.pdf(d1, 0, 1) * sigma / (2 * np.sqrt(T))) + r * K * np.exp(-r*T) * norm.cdf(-d2, 0, 1)
+        return theta/365
+    except:
+        print("Please confirm all option parameters!")
+
+def optionVega (S, K, r, T, sigma):
+    "Calculates option vega"
+    d1 = (np.log(S/K) + (r + sigma**2/2)* T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    
+    try:
+        vega = S * np.sqrt(T) * norm.pdf(d1, 0, 1) * 0.01
+        return vega
+    except:
+        print("Please confirm all option parameters!")
+
+def optionRho(S, K, r, T, sigma, type="c"):
+    "Calculates option rho"
+    d1 = (np.log(S/K) + (r + sigma**2/2)* T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    try:
+        if type == "c":
+            rho = 0.01 * K * T * np.exp(-r*T) * norm.cdf(d2, 0, 1)
+        elif type == "p":
+            rho = 0.01 * -K * T * np.exp(-r*T) * norm.cdf(-d2, 0, 1)
+        return rho
+    except:
+        print("Please confirm all option parameters!")
+
+print("Option Price: ", blackScholes(S, K, r, T, sigma, type="c"))
+# print("Option Delta: ", optionDelta(S, K, r, T, sigma, type="c"))
+# print("Option Gamma: ", optionGamma(S, K, r, T, sigma))
+# print("Option Theta: ", optionTheta(S, K, r, T, sigma, type="c"))
+# print("Option Vega: ", optionVega(S, K, r, T, sigma))
+# print("Option Rho: ", optionRho(S, K, r, T, sigma, type="c"))
+
+# days_array = [i for i in range(1, days_to_expiry + 1)]
+# days_array.sort(reverse=True)
+# underlying_price_var = [i for i in range(1, 101)]
+# strike_var = [i for i in range(1, 101)]
+# risk_free_var = np.linspace(0.0001, 0.25, 100)
+
+
+# variable = days_array
+
+# prices = []
+# deltas = []
+# gammas = []
+# thetas = []
+# vegas = []
+# rhos = []
+
+# if variable == days_array:
+#     for i in variable:
+#         prices.append(blackScholes(S, K, r, i, sigma, type))
+#         deltas.append(optionDelta(S, K, r, i, sigma, type))
+#         gammas.append(optionGamma(S, K, r, i, sigma))
+#         thetas.append(optionTheta(S,K, r, i, sigma, type))
+#         vegas.append(optionVega(S, K, r, i, sigma))
+#         rhos.append(optionRho(S, K, r, i, sigma, type))
+# elif variable == underlying_price_var:
+#     for i in variable:
+#         prices.append(blackScholes(i, K, r, T, sigma, type))
+#         deltas.append(optionDelta(i, K, r, T, sigma, type))
+#         gammas.append(optionGamma(i, K, r, T, sigma))
+#         thetas.append(optionTheta(i,K, r, T, sigma, type))
+#         vegas.append(optionVega(i, K, r, T, sigma))
+#         rhos.append(optionRho(i, K, r, T, sigma, type))
+# elif variable == strike_var:
+#     for i in variable:
+#         prices.append(blackScholes(S, i, r, T, sigma, type))
+#         deltas.append(optionDelta(S, i, r, T, sigma, type))
+#         gammas.append(optionGamma(S, i, r, T, sigma))
+#         thetas.append(optionTheta(S,i, r, T, sigma, type))
+#         vegas.append(optionVega(S, i, r, T, sigma))
+#         rhos.append(optionRho(S, i, r, T, sigma, type))
+# elif variable == risk_free_var:
+#     for i in variable:
+#         prices.append(blackScholes(S, K, i, T, sigma, type))
+#         deltas.append(optionDelta(S, K, i, T, sigma, type))
+#         gammas.append(optionGamma(S, K, i, T, sigma))
+#         thetas.append(optionTheta(S,K, i, T, sigma, type))
+#         vegas.append(optionVega(S, K, i, T, sigma))
+#         rhos.append(optionRho(S, K, i, T, sigma, type))
+
+# fig, ax = plt.subplots(2,3, figsize=(24,12))
+
+# ax[0, 0].plot(variable, prices)
+# ax[0, 0].set_title("Prices")
+
+# ax[0, 1].plot(variable, deltas)
+# ax[0, 1].set_title("Deltas")
+
+# ax[0, 2].plot(variable, gammas)
+# ax[0, 2].set_title("Gammas")
+
+# ax[1, 0].plot(variable, thetas)
+# ax[1, 0].set_title("Thetas")
+
+# ax[1, 1].plot(variable, vegas)
+# ax[1, 1].set_title("Vegas")
+
+# ax[1, 2].plot(variable, rhos)
+# ax[1, 2].set_title("Rhos")
+
+# if variable==days_array:
+#     ax[0,0].invert_xaxis()
+#     ax[0,1].invert_xaxis()
+#     ax[0,2].invert_xaxis()
+#     ax[1,0].invert_xaxis()
+#     ax[1,1].invert_xaxis()
+#     ax[1,2].invert_xaxis()
+
+# plt.show()
